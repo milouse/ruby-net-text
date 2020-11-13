b129308d4f46f53de7f883539fa95c6ad83b3e8f
Étienne Deparis <etienne@depar.is>
2020-11-13 13:16:08 +0100

    feat: Support finger URIs

---
+++ lib/uri/finger.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'uri'
+
+module URI
+  #
+  # The syntax of Finger URIs is defined in the Finger specification,
+  #   section 2.3: https://tools.ietf.org/html/rfc1288#section-2.3
+  #
+  class Finger < HTTP
+    # A Default port of 79 for URI::Finger.
+    DEFAULT_PORT = 79
+
+    # An Array of the available components for URI::Finger.
+    COMPONENT = %i[
+      scheme
+      userinfo
+      host port
+    ].freeze
+  end
+
+  @@schemes['FINGER'] = Finger
+end
